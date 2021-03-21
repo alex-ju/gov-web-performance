a5961dac3c34ee88990a317638c245be7e60c0ef
Alex <alex-ju@users.noreply.github.com>
2021-03-21 00:17:18 +0000

    Render graph

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var fs = require('fs');
+var countriesData = require('../data/countries.json');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var reports = [];
+  countriesData.countries.forEach((item, i) => {
+    try {
+      const reportString = fs.readFileSync(`./data/${item.tld}.json`);
+      const report = JSON.parse(reportString);
+      report.tld = item.tld;
+      report.name = item.name;
+      reports.push(report);
+    } catch(err) {
+      console.log(err);
+      return;
+    }
+  });
+  res.render('index', { title: 'Express', reports: reports });
 });
 
 module.exports = router;
